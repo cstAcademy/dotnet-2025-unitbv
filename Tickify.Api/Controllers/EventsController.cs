561c87c6a0f72342f6cca130d17020c24ea6a415
Alexandra Donici <adonici@transnetyx.com>
2025-05-14 10:51:14 +0300

    Revert "[Update] Rename get-events endpoint"
    This reverts commit 6bc3680afd7d3a2fbd816c94e3ed734f26fb81bc.

--- Tickify.Api/Controllers/EventsController.cs
+++ Tickify.Api/Controllers/EventsController.cs
@@ -16,8 +16,8 @@ namespace Tickify.Api.Controllers
             return Ok("Event added successfully");
         }
 
-        [HttpGet("get-filtered-events")]
-        public async Task<IActionResult> GetFilteredEvents()
+        [HttpGet("get-events")]
+        public async Task<IActionResult> GetEvents()
         {
             var result = await eventsService.GetEventsAsync();
             return Ok(result);
